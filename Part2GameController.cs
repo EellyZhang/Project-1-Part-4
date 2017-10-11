459138b0227ff0462693016688e58151c7c7888d
EellyZhang <31869183+EellyZhang@users.noreply.github.com>
2017-10-11 16:02:29 -0400

    Part 4

---
+++ Part2GameController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Part2GameController : MonoBehaviour
+{
+	int bronzePlayer;
+	int silverPlayer;
+	int goldPlayer;
+	public int miningSpeed;
+	int timeToMine;
+	public GameObject cubePrefab;
+
+	Vector3 cubePosition;
+	GameObject currentCube;
+
+	// Use this for initialization
+	void Start()
+	{
+
+		bronzePlayer = 0;
+		silverPlayer = 0;
+		goldPlayer = 0;
+		miningSpeed = 3;
+		timeToMine = miningSpeed;
+
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		if (Time.time > timeToMine)
+		{
+
+			if (bronzePlayer == 2 && silverPlayer == 2 && goldPlayer < 1)
+			{
+				currentCube = Instantiate(cubePrefab, new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), 0), Quaternion.identity);
+				currentCube.GetComponent<Renderer>().material.color = Color.yellow;
+
+				goldPlayer += 1;
+			}
+
+
+			else if (bronzePlayer < 4)
+			{
+				currentCube = Instantiate(cubePrefab, new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), 0), Quaternion.identity);
+				currentCube.GetComponent<Renderer>().material.color = Color.red;
+
+				bronzePlayer += 1;
+			}
+
+			else if (bronzePlayer >= 4)
+			{
+				currentCube = Instantiate(cubePrefab, new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), 0), Quaternion.identity);
+				currentCube.GetComponent<Renderer>().material.color = Color.white;
+
+				silverPlayer += 1;
+			}
+
+			print("Bronze: " + bronzePlayer + "Silver: " + silverPlayer + "Gold: " + goldPlayer);
+
+			timeToMine += miningSpeed;
+		}
+	}
+}
+
+
+
+
+// there is no way to spawn a gold ore. Is the gold part for Final? 
